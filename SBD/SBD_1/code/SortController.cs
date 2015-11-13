32a2e3707a1c0743ec53a9b1273143a5379efec5
Filip Biedrzycki <filip.biedrzycki@intel.com>
2015-11-13 23:16:58 +0100

    wip

---
+++ SBD/SBD_1/code/SortController.cs
@@ -0,0 +1,34 @@
+﻿using SBD;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBD_1.code
+{
+    static class SortController
+    {
+        internal static void Distribute(Tape src, CyclicList<Tape> dest)
+        {
+            using (StreamReader reader = new StreamReader(src.FilePath,))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    int val = int.Parse(line);
+                    if (val <= dest.LastValue)
+                    {
+                        dest.Add(val);
+                    }
+                    else
+                    {
+                        dest.ChangeToNextAndAdd(val);
+                    }
+
+                }
+            }
+        }
+    }
+}
